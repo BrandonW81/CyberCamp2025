ecfe07942e3738bb489d8d8e01976274196ffcd9
BrandonW81 <tllord65@gmail.com>
2025-07-18 01:00:01 -0400

    Update score.py

--- score.py
+++ score.py
@@ -1,4 +1,3 @@
-
 import os
 
 score = 0
@@ -11,6 +10,12 @@ def user_exists(username):
 def file_exists(filepath):
     return os.path.exists(filepath)
 
+def file_exists_anywhere(filename, search_path='/'):
+    for root, dirs, files in os.walk(search_path):
+        if filename in files:
+            return True
+    return False
+
 def main():
     global score
     if user_exists("ferb"):
@@ -19,12 +24,16 @@ def main():
         score += 20
     if not user_exists("doofenshmirtz"):
         score += 20
+    if user_exists("evil-intern"):
+        score += 20
     if not file_exists("/tmp/f"):
         score += 20
-    if not file_exists("ssh_keylogger.py"):
+    if not file_exists_anywhere("ssh_keylogger.py"):
+        score += 20
+    if file_exists_anywhere("lebron.png"):
         score += 20
 
-    print(f"You scored {score} points out of 100")
+    print(f"You scored {score} points.")
 
 if __name__ == "__main__":
     main()
